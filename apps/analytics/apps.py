d0d564a33cc413c3866c66364d53cf267bb7a994
Abderrahmane Kihal <abderrahmanekihal5@gmail.com>
2025-04-18 02:44:38 +0100

    add analytics module

--- apps/analytics/apps.py
+++ apps/analytics/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class AistormhackConfig(AppConfig):
+class AnalyticsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.authentication'
+    name = 'apps.analytics'
