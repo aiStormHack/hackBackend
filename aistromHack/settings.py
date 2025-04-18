a4690ed352d068a0a80bada69dffd09f33f85249
Abderrahmane Kihal <abderrahmanekihal5@gmail.com>
2025-04-18 02:46:29 +0100

    fix : apps references in settings.py

--- aistromHack/settings.py
+++ aistromHack/settings.py
@@ -24,6 +24,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework', 
+    'apps.analytics',
+    'apps.authentication'
+
 ]
 
 MIDDLEWARE = [
